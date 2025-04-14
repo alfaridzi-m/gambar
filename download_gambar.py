4219704f327a2fa967e7f5873423d11265699ac4
hdplx <alfaridzi@outlook.com>
2025-04-14 23:11:58 +0700

    perubahan besar

--- download_gambar.py
+++ download_gambar.py
@@ -5,6 +5,7 @@ import datetime
 import os
 import winsound
 import shutil
+import requests
 
 
 try:
@@ -45,7 +46,7 @@ print("=====================================")
 current_dir = os.getcwd()
 folder_path = os.path.join(current_dir, tanggal)
 
-print(f"Current dir: {current_dir}")
+print(f"Lokasi saat ini di : {current_dir}")
 if os.path.exists(folder_path):
     print(f"Folder {tanggal} sudah ada")
     shutil.rmtree(tanggal)
@@ -71,99 +72,139 @@ def program1(tanggal,tanggal2):
         ifs = "https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/ecmwf/prakiraan/Backup/RAIN/rainrate_ifs0p125_sfc_" + tanggal + jam1 + "0000.png"
         # print(ifs)
         try:
-            filenameonly = os.path.basename(ifs)
-            print(fr"Sedang Download {filenameonly}")
-            opener.retrieve(ifs, fr'ifs' + jam1 + '_' + tanggal + '.webp')
-            print(fr"ifs" + jam1 + '_' + tanggal + '.png' + " sudah di download")
+            filename = os.path.basename(ifs)
+            filenamewithoutext = os.path.splitext(filename)[0]
+            opener.retrieve(ifs, fr'{filenamewithoutext}'+'.png')
+            print(f'{filename} sudah di download')
         except:
-            print(fr"Sepertinya link IFS {tanggal}{jam1} gagal di download")
+            print(fr"{filename} gagal di download")
 
     jam02 = "https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/ecmwf/prakiraan/Backup/RAIN/rainrate_ifs0p125_sfc_" + tanggal2 + "020000.png"
     try:
-        filenameonly = os.path.basename(jam02)
-        print(fr"Sedang Download {filenameonly}")
-        opener.retrieve(jam02, fr'ifs' + "26" + '_' + tanggal + '.png')
-        print(fr'ifs' + "26" + '_' + tanggal + '.webp' +  "sudah di download")
+        filename = os.path.basename(jam02)
+        filenamewithoutext = os.path.splitext(filename)[0]
+        opener.retrieve(jam02, fr'{filenamewithoutext}'+'.png')
+        print(f'{filename} sudah di download')
     except:
-        print(fr"Sepertinya link IFS 26_{tanggal} gagal di download")
+        print(fr"{filename} gagal di download")
         
     j2 = ['01','07','13','19']
     for jam2 in j2:
         wrf = "https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/wrf/prakiraan/RAIN/rainrate_wrf10km_sfc_" + tanggal + jam2 + "0000.png"
         # print(wrf)
         try:
-            filenameonly = os.path.basename(wrf)
-            print(fr"Sedang Download {filenameonly}")
-            opener.retrieve(wrf, fr'wrf' + jam2 + '_' + tanggal + '.png')
-            print(fr'wrf' + jam2 + '_' + tanggal + '.png' + " sudah di download")
+            filename = os.path.basename(wrf)
+            filenamewithoutext = os.path.splitext(filename)[0]
+            opener.retrieve(wrf, fr'{filenamewithoutext}'+'.png')
+            print(f'{filename} sudah di download')
         except:
-            print(fr"Sepertinya link wrf {tanggal}{jam2} gagal di download")
+            print(fr"{filename} gagal di download")
 
     angin = "https://web-meteo.bmkg.go.id//media/data/bmkg/Angin3000ft/Streamline_" + tanggal + "070000.jpg"
     satelit = "http://202.90.198.22/IMAGE/HIMA/H08_EH_Sulsel.png"
     sst = "http://web-meteo.bmkg.go.id//media/data/bmkg/mfy/ecmwf/Analisis/sst_anal_sea_dy.png"
     sstano = "http://web-meteo.bmkg.go.id//media/data/bmkg/mfy/ecmwf/Analisis/ssta_anal_sea_dy.png"
-    ibf00 = "https://cdn.bmkg.go.id/MEWS/ibf/27_sulsel_00.png"
-    ibf24 = "https://cdn.bmkg.go.id/DataMKG/MEWS/27_sulsel_24.png"
-    ibf48 = "https://cdn.bmkg.go.id/DataMKG/MEWS/27_sulsel_48.png"
 
-    try:
-        opener.retrieve(angin, fr'\w_' + tanggal + '.jpg')
-        opener.retrieve(sst, fr'\SST_' + tanggal + '.jpg')
-        opener.retrieve(sstano, fr'\SSTANO_' + tanggal + '.jpg')
-        opener.retrieve(ibf00, fr'\ibf00.jpg')
-        opener.retrieve(ibf24, fr'\ibf24_KIRIM KE GRUP.jpg')
-        opener.retrieve(ibf48, fr'\ibf48_KIRIM KE GRUP.jpg')
-    except:
-        print(color.BOLD + color.RED + "Angin " + tanggal + " gagal data belum tersedia" + color.END)
+    # Loop through the list of URLs and download each file
+    for url in [angin, satelit, sst, sstano]:
+        try:
+            filename = os.path.basename(url)
+            response = requests.get(url, allow_redirects=True)
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+            print(f'{filename} sudah di download')
+        except Exception as e:
+            print(f'Gagal mendownload {url}: {e}')
 
-    try:
-        opener.retrieve(satelit, fr'\Sulsel ' + sekarang + '.png')
-    except:
-        print(color.BOLD + color.RED + "Satelit gagal link salah tidak terdaftar" + color.END)
+def bomfile():
+	rh850_00= "http://www.bom.gov.au/charts_data/IDY20108/current/RH/850hPa/IDY20108.RH-850hPa.012.png"
+	rh850_12 = "http://www.bom.gov.au/charts_data/IDY20108/current/RH/850hPa/IDY20108.RH-850hPa.024.png"
+	rh700_00= "http://www.bom.gov.au/charts_data/IDY20108/current/RH/700hPa/IDY20108.RH-700hPa.012.png"
+	rh700_12 = "http://www.bom.gov.au/charts_data/IDY20108/current/RH/700hPa/IDY20108.RH-700hPa.024.png"
+	rh500_00= "http://www.bom.gov.au/charts_data/IDY20108/current/RH/500hPa/IDY20108.RH-500hPa.024.png"
+	rh500_12 = "http://www.bom.gov.au/charts_data/IDY20108/current/RH/500hPa/IDY20108.RH-500hPa.012.png"
+	rr_t12 = "http://www.bom.gov.au/charts_data/IDY20108/current/mslp-precip/IDY20108.mslp-precip.012.png"
+	rr_t18 = "http://www.bom.gov.au/charts_data/IDY20108/current/mslp-precip/IDY20108.mslp-precip.018.png"
+	rr_t24 = "http://www.bom.gov.au/charts_data/IDY20108/current/mslp-precip/IDY20108.mslp-precip.024.png"
+	rr_t30 = "http://www.bom.gov.au/charts_data/IDY20108/current/mslp-precip/IDY20108.mslp-precip.030.png"
 
+	# Loop through the list of BOM URLs and download each file
+	for url in [rr_t12, rr_t18, rr_t24, rr_t30, rh850_00, rh850_12, rh700_00, rh700_12, rh500_00, rh500_12]:
+		try:
+			filename = os.path.basename(url)
+			filenamewithoutext = os.path.splitext(filename)[0]
+			opener.retrieve(url, fr'{filenamewithoutext}'+'.png')
+			print(f'{filename} sudah di download')
+		except Exception as e:
+			print(f'Gagal mendownload {url}: {e}')
 
 def program2(tanggal,tanggal2):
     directory = str(tanggal)
-    os.mkdir(fr'\modelifs{directory}')
-    os.mkdir(fr'\modelwrf{directory}')
-    os.mkdir(fr'\esembeldanprobabilistic{directory}')
+    os.mkdir(fr'{current_dir}\{tanggal}\modelifs{directory}')
+    os.mkdir(fr'{current_dir}\{tanggal}\modelwrf{directory}')
+    os.mkdir(fr'{current_dir}\{tanggal}\esembeldanprobabilistic{directory}')
 
     #Rain Rate hourly
-    j = ['08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23']
+    j = [f'{hour:02}' for hour in range(8, 24)]
     for jam1 in j:
-        ifs = "https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/ecmwf/prakiraan/Backup/RAIN/rainrate_ifs0p125_sfc_" + tanggal + jam1 + "0000.png"
-        wrf = "https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/wrf/prakiraan/RAIN/rainrate_wrf10km_sfc_"+ tanggal + jam1 + "0000.png"
+        ifs = f"https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/ecmwf/prakiraan/Backup/RAIN/rainrate_ifs0p125_sfc_{tanggal}{jam1}0000.png"
+        wrf = f"https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/wrf/prakiraan/RAIN/rainrate_wrf10km_sfc_{tanggal}{jam1}0000.png"
         try:
-            opener.retrieve(ifs, fr'\modelifs{directory}\ifs' + jam1 + '_' + tanggal + '.png')
-            opener.retrieve(wrf,fr'\modelwrf{directory}\wrf' + jam1 + '_' + tanggal + '.png')
-        except:
-            print(fr"Sepertinya link IFS atau wrf {tanggal}{jam1} gagal di download")
+            filenameonlywrf = os.path.basename(wrf)
+            filenamewithoutextwrf = os.path.splitext(filenameonlywrf)[0]
+            opener.retrieve(wrf, fr"{current_dir}\{tanggal}\modelwrf{directory}\{filenamewithoutextwrf}" + '.png')
+            print(fr"{filenamewithoutextwrf} sudah di download")
+        except Exception as e:
+            print(fr"{filenamewithoutextwrf} gagal di download : {e}")
+        
+        try:
+            filenameonlyifs = os.path.basename(ifs)
+            filenamewithoutextifs = os.path.splitext(filenameonlyifs)[0]
+            opener.retrieve(ifs, fr"{current_dir}\{tanggal}\modelifs{directory}\{filenamewithoutextifs}" + '.png')
+            print(fr"{filenamewithoutextifs} sudah di download")
+        except Exception as e:
+            print(fr"{filenamewithoutextifs} gagal di download : {e}")
 
-    xx = ['00', '01', '02', '03', '04', '05', '06', '07']
+    xx = [f'{hour:02}' for hour in range(8)]
     for k in xx:
-        jam02 = "https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/ecmwf/prakiraan/Backup/RAIN/rainrate_ifs0p125_sfc_" + tanggal2 + k + "0000.png"
-        jam03 = "https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/wrf/prakiraan/RAIN/rainrate_wrf10km_sfc_" + tanggal + k + "0000.png"
-        t = int(k) + 24
-        t = str(t)
+        ifsxx = f"https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/ecmwf/prakiraan/Backup/RAIN/rainrate_ifs0p125_sfc_{tanggal2}{k}0000.png"
+        wrfxx = f"https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/wrf/prakiraan/RAIN/rainrate_wrf10km_sfc_{tanggal}{k}0000.png"
+
         try:
-            opener.retrieve(jam02, fr'\modelifs{directory}\ifs' + t + '_' + tanggal + '.png')
-            opener.retrieve(jam03, fr'\modelwrf{directory}\wrf' + k + '_' + tanggal + '.png')
-        except:
-            print(fr"Sepertinya link RR IFS atau wrf {tanggal}{k} gagal di download")
+            filenameonlywrf = os.path.basename(wrfxx)
+            filenamewithoutextwrf = os.path.splitext(filenameonlywrf)[0]
+            opener.retrieve(wrfxx, fr"{current_dir}\{tanggal}\modelwrf{directory}\{filenamewithoutextwrf}" + '.png')
+            print(fr"{filenamewithoutextwrf} sudah di download")
+        except Exception as e:            print(fr"{filenamewithoutextwrf} gagal di download : {e}")
+        
+        try:
+            filenameonlyifs = os.path.basename(ifsxx)
+            filenamewithoutextifs = os.path.splitext(filenameonlyifs)[0]
+            opener.retrieve(ifsxx, fr"{current_dir}\{tanggal}\modelifs{directory}\{filenamewithoutextifs}" + '.png')
+            print(fr"{filenamewithoutextifs} sudah di download")
+        except Exception as e:            print(fr"{filenamewithoutextifs} gagal di download : {e}")
 
     #RH
-    j = ['00', '03', '06', '09', '12', '15', '18', '21']
+    j =  ['00', '03', '06', '09', '12', '15', '18', '21']
     for jam1 in j:
         lapisan = ["850", "700", "500"]
         for lapisan2 in lapisan:
-            rhifs = "https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/ecmwf/prakiraan/Backup/RH/rh_ifs0p125_"+ lapisan2 + "mb_" + tanggal + jam1 + "0000.png"
-            rhwrf = "https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/wrf/prakiraan/RH/rh_wrf10km_"+ lapisan2 + "mb_" + tanggal + jam1 + "0000.png"
+            rhifs = f"https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/ecmwf/prakiraan/Backup/RH/rh_ifs0p125_{lapisan2}mb_{tanggal}{jam1}0000.png"
+            rhwrf = f"https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/wrf/prakiraan/RH/rh_wrf10km_{lapisan2}mb_{tanggal}{jam1}0000.png"
             try:
-                opener.retrieve(rhifs, fr'\modelifs{directory}\rhifs' + lapisan2 + "_" + jam1 + '_' + tanggal + '.png')
-                opener.retrieve(rhwrf, fr'\modelwrf{directory}\rhwrf'  + lapisan2 + "_" + jam1 + '_' + tanggal + '.png')
-            except:
-                print(fr"Sepertinya link rh IFS atau wrf {tanggal}{jam1} lapisan {lapisan2}gagal di download")
+                filenameonlyifs = os.path.basename(rhifs)
+                filenamewithoutextifs = os.path.splitext(filenameonlyifs)[0]
+                opener.retrieve(rhifs, fr"{current_dir}\{tanggal}\modelifs{directory}\{filenamewithoutextifs}" + '.png')
+                print(fr"{filenamewithoutextifs} sudah di download")
+            except Exception as e:
+                print(fr"{filenamewithoutextifs} gagal di download : {e}")
+            try:
+                filenameonlywrf = os.path.basename(rhwrf)
+                filenamewithoutextwrf = os.path.splitext(filenameonlywrf)[0]
+                opener.retrieve(rhwrf, fr"{current_dir}\{tanggal}\modelwrf{directory}\{filenamewithoutextwrf}" + '.png')
+                print(fr"{filenamewithoutextwrf} sudah di download")
+            except Exception as e:
+                print(fr"{filenamewithoutextwrf} gagal di download : {e}")
 
     #indeks
     j = ['00', '03', '06', '09', '12', '15', '18', '21']
@@ -171,65 +212,76 @@ def program2(tanggal,tanggal2):
         indeks = ["KI", "LI", "SI"]
         for indeks2 in indeks:
             indeks3 = indeks2.lower()
-            labilifs = "https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/ecmwf/prakiraan/Backup/"+indeks2+"/"+indeks3 +"_ifs0p125_sfc_" + tanggal + jam1 + "0000.png"
-            labilifswrf = "https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/wrf/prakiraan/"+indeks2+"/"+indeks3+"_wrf10km_sfc_" + tanggal + jam1 + "0000.png"
+            labilifs = f"https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/ecmwf/prakiraan/Backup/{indeks2}/{indeks3}_ifs0p125_sfc_{tanggal}{jam1}0000.png"
+            labilifswrf = f"https://web-meteo.bmkg.go.id//media/data/bmkg/mfy/wrf/prakiraan/{indeks2}/{indeks3}_wrf10km_sfc_{tanggal}{jam1}0000.png"
             try:
-                opener.retrieve(labilifs,fr'\modelifs{directory}\IFS' + indeks2 + "_" + jam1 + '_' + tanggal + '.png')
-                opener.retrieve(labilifswrf,fr'\modelwrf{directory}\WRF' + indeks2 + "_" + jam1 + '_' + tanggal + '.png')
-            except:
-                print(f"indeks {indeks2} wrf jam {jam1} kosong")
+                filenameonlyifs = os.path.basename(labilifs)
+                filenamewithoutextifs = os.path.splitext(filenameonlyifs)[0]
+                opener.retrieve(labilifs, fr"{current_dir}\{tanggal}\modelifs{directory}\{filenamewithoutextifs}" + '.png')
+                print(fr"{filenamewithoutextifs} sudah di download")
+            except Exception as e:
+                print(fr"{filenamewithoutextifs} gagal di download : {e}")
+            try:
+                filenameonlywrf = os.path.basename(labilifswrf)
+                filenamewithoutextwrf = os.path.splitext(filenameonlywrf)[0]
+                opener.retrieve(labilifswrf, fr"{current_dir}\{tanggal}\modelwrf{directory}\{filenamewithoutextwrf}" + '.png')
+                print(fr"{filenamewithoutextwrf} sudah di download")
+            except Exception as e:
+                print(fr"{filenamewithoutextwrf} gagal di download : {e}")
 
     #probabilistic dan esembel model
-    poe20 = "https://web-meteo.bmkg.go.id//media/data/bmkg/probabilistic//POE20_24hrprec_" + tanggal + "000000_ifs.png"
-    poe10 = "https://web-meteo.bmkg.go.id//media/data/bmkg/probabilistic//POE10_24hrprec_" + tanggal + "000000_ifs.png"
-    poe24 = "https://web-meteo.bmkg.go.id//media/data/bmkg/probabilistic//POE5_24hrprec_" + tanggal + "000000_ifs.png"
-    emeanaccessgt = "https://web-meteo.bmkg.go.id//media/data/bmkg/probabilistic//Emean_24hrprec_" + tanggal + "000000_accessgt.png"
-    emeangefs = "https://web-meteo.bmkg.go.id//media/data/bmkg/probabilistic//Emean_24hrprec_" + tanggal + "000000_gefs.png"
-    emeanifs = "https://web-meteo.bmkg.go.id//media/data/bmkg/probabilistic//Emean_24hrprec_" + tanggal + "000000_ifs.png"
+    poe20 = f"https://web-meteo.bmkg.go.id//media/data/bmkg/probabilistic//POE20_24hrprec_{tanggal}000000_ifs.png"
+    poe10 = f"https://web-meteo.bmkg.go.id//media/data/bmkg/probabilistic//POE10_24hrprec_{tanggal}000000_ifs.png"
+    poe5 = f"https://web-meteo.bmkg.go.id//media/data/bmkg/probabilistic//POE5_24hrprec_{tanggal}000000_ifs.png"
+    emeanaccessgt = f"https://web-meteo.bmkg.go.id//media/data/bmkg/probabilistic//Emean_24hrprec_{tanggal}000000_accessgt.png"
+    emeangefs = f"https://web-meteo.bmkg.go.id//media/data/bmkg/probabilistic//Emean_24hrprec_{tanggal}000000_gefs.png"
+    emeanifs = f"https://web-meteo.bmkg.go.id//media/data/bmkg/probabilistic//Emean_24hrprec_{tanggal}000000_ifs.png"
 
-    # bom850_00= "http://www.bom.gov.au/charts_data/IDY20108/current/RH/850hPa/IDY20108.RH-850hPa.012.png?1716465600"
-    # bom850_12 = "http://www.bom.gov.au/charts_data/IDY20108/current/RH/850hPa/IDY20108.RH-850hPa.024.png?1716465600"
-    # bom700_00= "http://www.bom.gov.au/charts_data/IDY20108/current/RH/700hPa/IDY20108.RH-700hPa.012.png?1716465600"
-    # bom700_12 = "http://www.bom.gov.au/charts_data/IDY20108/current/RH/700hPa/IDY20108.RH-700hPa.024.png?1716465600"
-    # bom500_00= "http://www.bom.gov.au/charts_data/IDY20108/current/RH/500hPa/IDY20108.RH-500hPa.024.png?1716465600"
-    # bom500_12 = "http://www.bom.gov.au/charts_data/IDY20108/current/RH/500hPa/IDY20108.RH-500hPa.012.png?1716465600"
-    #
-    # bom = [bom500_00, bom500_12, bom700_12, bom700_00, bom850_12, bom850_00]
-    # for zz in bom:
-    #     try:
-    #         opener.retrieve(zz,fr'\{zz}_{tanggal}.png')
-    #     except:
-    #         print(fr"{zz} Belum ada ")
-
-    uu = [poe20,poe10,poe24]
+    uu = [poe20,poe10,poe5]
     for nn in uu:
         try:
-            opener.retrieve(nn,fr'\esembeldanprobabilistic{directory}\{nn[ta]}.png')
-        except:
-            print(fr"{nn} Belum ada ")
+            filenameonly = os.path.basename(nn)
+            filenamewithoutext = os.path.splitext(filenameonly)[0]
+            opener.retrieve(nn, fr"{current_dir}\{tanggal}\esembeldanprobabilistic{directory}\{filenamewithoutext}" + '.png')
+            print(fr"{filenamewithoutext} sudah di download")
+        except Exception as e:            
+            print(fr"{filenamewithoutext} gagal di download : {e}")
 
     yy = [emeanifs, emeangefs, emeanaccessgt]
     for nn in yy:
         try:
-            opener.retrieve(nn,fr'\esembeldanprobabilistic{directory}\{nn[91:95]}.png')
-        except:
-            print(fr"{nn} Belum ada ")
+            filenameonly = os.path.basename(nn)
+            filenamewithoutext = os.path.splitext(filenameonly)[0]
+            opener.retrieve(nn, fr"{current_dir}\{tanggal}\esembeldanprobabilistic{directory}\{filenamewithoutext}" + '.png')
+            print(fr"{filenamewithoutext} sudah di download")
+        except Exception as e:            
+            print(fr"{filenamewithoutext} gagal di download : {e}")
 
-    inawp = "https://web-meteo.bmkg.go.id//media/data/bmkg//inanwp//InaNWP_RR24hr_Indo_" + tanggal2 + "000000.png"
-    try:
-        opener.retrieve(inawp, fr'\inawp{tanggal2}.png')
-    except:
-        print(fr"Inawp belum ada Belum ada ")
 while True:
+    print("=====================================")
+    print("Proses download file yang diperlukan analisa")
+    time.sleep(1)
+    print("=====================================")
     program1(tanggal,tanggal2)
+    print("File analisa telah selesai di download")
+    print("=====================================")
+    print("Proses download file tambahan")
     program2(tanggal, tanggal2)
+    print("Selesai download file tambahan")
+    time.sleep(1)
+    print("=====================================")
+    print("Proses download file BOM")
+    bomfile()
+    print("=====================================")
+    print("Proses download file BOM telah selesai")
     end = time.time() - start
     print(fr"Selesai dalam {end} detik")
     winsound.Beep(6000, 100)
     winsound.Beep(6000, 100)
     winsound.Beep(6000, 100)
     input("Tekan Enter untuk membuka folder")
-    os.startfile(fr"{currnet_dir}\{tanggal}")
+    os.chdir(current_dir)
+    os.startfile(fr"{current_dir}\{tanggal}")
     break
 
 
